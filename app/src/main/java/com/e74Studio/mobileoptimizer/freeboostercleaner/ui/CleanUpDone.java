f53c735c9a672917ff60f09c509a6c7afe2a9aad
edzapodka <edzapodka@gmail.com>
2018-03-09 22:22:55 +0700

    Tambah File

---
+++ app/src/main/java/com/e74Studio/mobileoptimizer/freeboostercleaner/ui/CleanUpDone.java
@@ -0,0 +1,14 @@
+package com.e74Studio.mobileoptimizer.freeboostercleaner.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.e74Studio.mobileoptimizer.freeboostercleaner.R;
+
+
+public class CleanUpDone extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cleanup_done);
+    }
+}
